f048f9f7f4f772d22acc4a5b62eb0b5192b3893d
Michael Davis <mdavis332@gmail.com>
2018-07-26 14:44:02 -0500

    Support web calls
    System.Web.HttpUtility type accelerator no longer works without explicitly adding the System.Web type in advance, so I've added that call.
    Additionally, fixed invoke-webrequest not working against an RT server with TLS 1.2.

--- PSRT/Public/New-RTSession.ps1
+++ PSRT/Public/New-RTSession.ps1
@@ -38,6 +38,8 @@ function New-RTSession {
 
         [switch]$DontUpdateConfig
     )
+    Add-Type -AssemblyName System.Web
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
     Write-Verbose "Creating RT Session"
     $EncodedUserName = [System.Web.HttpUtility]::UrlEncode($Credential.UserName)
     $EncodedPassword = [System.Web.HttpUtility]::UrlEncode($Credential.GetNetworkCredential().Password)
